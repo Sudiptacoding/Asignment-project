b30e145be0a3ec0011c8f065139674d4c5cb3023
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-04-12 10:17:32 +0600

    asignment

---
+++ src/Components/Blog/Blog.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Blog = () => {
+    const styl = {
+        
+    }
+    return (
+        <div style={{width:'100%' , display:'flex' , alignItems:'center' , justifyContent:'center' , flexWrap:'wrap' , height:'100vh' , fontSize:'50px'}}>
+            <h1>No Content</h1>
+        </div>
+    );
+};
+
+export default Blog;
